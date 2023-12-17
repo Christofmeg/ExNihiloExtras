b3b428e79e5d1bf86a408928c20325414b5b00f0
Christofmeg <4357405+Christofmeg@users.noreply.github.com>
2023-12-17 09:55:08 +0100

    Quilt (rework needed)

--- Quilt/src/main/java/com/christofmeg/exnihiloextras/ExNihiloExtras.java
+++ Quilt/src/main/java/com/christofmeg/exnihiloextras/ExNihiloExtras.java
@@ -2,9 +2,9 @@ package com.christofmeg.exnihiloextras;
 
 import com.christofmeg.exnihiloextras.init.ItemRegistration;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
-import net.minecraft.item.ItemGroup;
-import net.minecraft.item.ItemStack;
-import net.minecraft.util.Identifier;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import org.quiltmc.loader.api.ModContainer;
 import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
 
@@ -12,8 +12,8 @@ public class ExNihiloExtras implements ModInitializer {
 
     public static final String MOD_ID = "exnihiloextras";
 
-    public static final ItemGroup ITEM_GROUP = FabricItemGroup.builder(
-            new Identifier(MOD_ID, "general"))
+    public static final CreativeModeTab ITEM_GROUP = FabricItemGroup.builder(
+            new ResourceLocation(MOD_ID, "general"))
             .icon(() -> new ItemStack(ItemRegistration.DOLL_FROG)).build();
 
     @Override
--- Quilt/src/main/java/com/christofmeg/exnihiloextras/init/ItemRegistration.java
+++ Quilt/src/main/java/com/christofmeg/exnihiloextras/init/ItemRegistration.java
@@ -2,14 +2,14 @@ package com.christofmeg.exnihiloextras.init;
 
 import com.christofmeg.exnihiloextras.ExNihiloExtras;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
-import net.minecraft.client.item.TooltipContext;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.text.Text;
-import net.minecraft.util.Identifier;
-import net.minecraft.world.World;
+import net.minecraft.core.Registry;
+import net.minecraft.core.registries.BuiltInRegistries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
+import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import org.quiltmc.qsl.item.setting.api.QuiltItemSettings;
@@ -20,33 +20,33 @@ public class ItemRegistration {
 
     public static final Item DOLL_BEE = new Item(new QuiltItemSettings()) {
         @Override
-        public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context) {
-            tooltip.add(Text.translatable(ExNihiloExtras.MOD_ID + ".tooltip.doll.bee"));
+        public void appendHoverText(@NotNull ItemStack stack, @Nullable Level world, List< Component > tooltip, @NotNull TooltipFlag context) {
+            tooltip.add(Component.translatable(ExNihiloExtras.MOD_ID + ".tooltip.doll.bee"));
         }
     };
 
     public static final Item DOLL_FROG = new Item(new QuiltItemSettings()) {
         @Override
-        public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
-            tooltip.add(Text.translatable(ExNihiloExtras.MOD_ID + "." + "tooltip.doll.frog"));
+        public void appendHoverText(@NotNull ItemStack stack, @Nullable Level world, List< Component > tooltip, @NotNull TooltipFlag context) {
+            tooltip.add(Component.translatable(ExNihiloExtras.MOD_ID + "." + "tooltip.doll.frog"));
 
         }
     };
 
     public static final Item DOLL_PANDA = new Item(new QuiltItemSettings()) {
         @Override
-        public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
-            tooltip.add(Text.translatable(ExNihiloExtras.MOD_ID + ".tooltip.doll.panda"));
+        public void appendHoverText(@NotNull ItemStack stack, @Nullable Level world, List< Component > tooltip, @NotNull TooltipFlag context) {
+            tooltip.add(Component.translatable(ExNihiloExtras.MOD_ID + ".tooltip.doll.panda"));
         }
     };
 
     public static void registerItems() {
-        Registry.register(Registries.ITEM, new Identifier(ExNihiloExtras.MOD_ID, "doll_bee"), DOLL_BEE);
-        Registry.register(Registries.ITEM, new Identifier(ExNihiloExtras.MOD_ID, "doll_frog"), DOLL_FROG);
-        ItemGroupEvents.modifyEntriesEvent(ExNihiloExtras.ITEM_GROUP).register(entries -> {
-            entries.add(DOLL_BEE);
-            entries.add(DOLL_FROG);
-            entries.add(DOLL_PANDA);
+        Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(ExNihiloExtras.MOD_ID, "doll_bee"), DOLL_BEE);
+        Registry.register(BuiltInRegistries.ITEM, new ResourceLocation(ExNihiloExtras.MOD_ID, "doll_frog"), DOLL_FROG);
+        ItemGroupEvents.modifyEntriesEvent(ExNihiloExtras.ITEM_GROUP).register((items) -> {
+            items.accept(DOLL_BEE);
+            items.accept(DOLL_FROG);
+            items.accept(DOLL_PANDA);
         });
     }
 
